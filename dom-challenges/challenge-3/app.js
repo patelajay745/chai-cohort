20933ca11a27409755ba252cdf03dd41dcffed15
Ajay Patel <patel.ajay745@gmail.com>
2025-02-22 19:52:22 -0500

    dom Challenge 3 completed

--- dom-challenges/challenge-3/app.js
+++ dom-challenges/challenge-3/app.js
@@ -1,3 +1,30 @@
 /**
  * Write your challenge solution here
  */
+
+// nameDisplay
+// jobDisplay
+//ageDisplay
+// bioDisplay
+
+// nameInput
+document.getElementById("nameInput").addEventListener("input", () => {
+  document.getElementById("nameDisplay").textContent =
+    document.getElementById("nameInput").value;
+});
+// jobInput
+document.getElementById("jobInput").addEventListener("input", () => {
+  document.getElementById("jobDisplay").textContent =
+    document.getElementById("jobInput").value;
+});
+// ageInput
+document.getElementById("ageInput").addEventListener("input", () => {
+  document.getElementById("ageDisplay").textContent =
+    document.getElementById("ageInput").value;
+});
+// bioInput
+document.getElementById("bioInput").addEventListener("input", () => {
+  document.getElementById("bioDisplay").textContent =
+    document.getElementById("bioInput").value;
+});
+//
