58c06e3cb9dad86601c2fa66729bb2fb5452de68
Ajay Patel <patel.ajay745@gmail.com>
2025-03-05 07:11:00 -0500

    completed dom challenge 7

--- dom-challenges/6-10/challenge-7/app.js
+++ dom-challenges/6-10/challenge-7/app.js
@@ -1,3 +1,26 @@
 /**
  * Write your challenge solution here
  */
+
+const btnToOpenClose = document.querySelectorAll(".accordion-button");
+const allAccordionItem = document.querySelectorAll(".accordion-item");
+
+console.log(btnToOpenClose);
+
+btnToOpenClose.forEach((button) => {
+  button.addEventListener("click", function () {
+    // console.log(button.parentElement);
+    const accordionItem = button.parentElement;
+
+    if (accordionItem.classList.contains("active")) {
+      accordionItem.classList.remove("active");
+      return;
+    }
+
+    allAccordionItem.forEach((item) => {
+      item.classList.remove("active");
+    });
+
+    accordionItem.classList.add("active");
+  });
+});
