2480dabc5f01771b167d74610d72091f86dfd165
Ajay Patel <patel.ajay745@gmail.com>
2025-03-05 08:28:01 -0500

    completed dom challenge 9

--- dom-challenges/6-10/challenge-9/app.js
+++ dom-challenges/6-10/challenge-9/app.js
@@ -1,3 +1,29 @@
 /**
  * Write your challenge solution here
  */
+
+const toogleBtn = document.querySelector(".toggle-btn");
+const panel = document.querySelector(".panel");
+const content = document.querySelector(".content");
+const menuItems = document.querySelectorAll(".menu-item");
+const closeBtnElement = document.querySelector(".close-btn");
+
+toogleBtn.addEventListener("click", function (event) {
+  !panel.classList.contains("active") ? panel.classList.add("active") : null;
+
+  event.stopPropagation();
+});
+
+content.addEventListener("click", function () {
+  panel.classList.remove("active");
+});
+
+menuItems.forEach((item) => {
+  item.addEventListener("click", function () {
+    alert(item.textContent);
+  });
+});
+
+closeBtnElement.addEventListener("click", function () {
+  panel.classList.remove("active");
+});
